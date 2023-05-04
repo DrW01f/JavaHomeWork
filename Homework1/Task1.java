57eebc2c1606250997089e79cff904b6ccb59d37
Andrey <Dr_Wo1f@mail.ru>
2023-05-04 19:48:01 +0300

    task 2 finished

--- Homework1/Task1.java
+++ Homework1/Task1.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Task1 {
     // Вычислить n-ое треугольного число (сумма чисел от 1 до n)
-    public static void main(String[] args){
+    public static void main(String[] args) {
         calculations();
-    } 
+    }
 
-    public static void calculations(){
+    public static void calculations() {
         System.out.println("Введите число: ");
         Scanner scanner = new Scanner(System.in);
-        String number = scanner.nextLine();
-        int result = 0; 
-        for (int i = 0; i <= Integer.parseInt(number); i++) {
+        int number = Integer.parseInt(scanner.nextLine());
+        int result = 0;
+        for (int i = 0; i <= number; i++) {
             result += i;
         }
-        
+
         System.out.println("Результат равен: " + result);
 
     }
--- Homework1/Task2.java
+++ Homework1/Task2.java
@@ -1,5 +1,24 @@
-
+import java.util.Scanner;
 
 public class Task2 {
-    
+    // Вычислить n! (произведение чисел от 1 до n)
+    public static void main(String[] args){
+        calculations();
+    }   
+
+    public static void calculations() {
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int result = 1; //для умножения 1
+        if (number == 0) {
+            System.out.println("Результат равен 0");
+        }
+        else{
+            for (int i = 1; i <= number; i++) {
+                result *= i;
+            }
+            System.out.println("Результат равен: " + result);
+        }
+    }
 }
