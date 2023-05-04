2e2d4a9e4314a52e34736d4d39a1d41d5f42af40
Andrey <Dr_Wo1f@mail.ru>
2023-05-04 22:30:50 +0300

    task 3 finished

---
+++ Homework1/Task3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Task3 {
+    /*
+     * Вывести все простые числа от 1 до 1000 (числа, которые делятся только на 1 и
+     * на
+     * себя без остатка)
+     */
+    public static void main(String[] args) {
+        int number = inputFunc();
+        System.out.println(findSimpleNumbers(number));
+    }
+
+    public static int inputFunc() {
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        return number;
+    }
+
+    public static String findSimpleNumbers(int number) {
+        String trickyOut = "";
+        int numberOfDivisions = 0;
+        for (int i = 1; i <= number; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    numberOfDivisions++;
+                }
+            }
+            if (numberOfDivisions <= 2) {
+                trickyOut += Integer.toString(i) + ", ";
+            }        
+            numberOfDivisions = 0;
+        }
+        return trickyOut;
+    }
+}
