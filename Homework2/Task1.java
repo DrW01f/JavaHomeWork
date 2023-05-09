9ef47c9adca094f942a4c1ea12a6547f16224450
Andrey <Dr_Wo1f@mail.ru>
2023-05-09 11:59:46 +0300

    in progress

--- Homework2/Task1.java
+++ Homework2/Task1.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 
 public class Task1 {
     public static void main(String[] args) {
@@ -14,13 +18,49 @@ public class Task1 {
 как replace, split, substring и т.д. по необходимости
 Создать метод, который запишет результат работы в файл. Обработайте исключения и 
 запишите ошибки в лог файл.
-2. *Получить исходную json строку из файла, используя FileReader или Scanner
-3. *Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой 
-итерации запишите в лог-файл. */       
+2. *Получить исходную json строку из файла, используя FileReader или Scanner */       
+String str =  readFile();
+System.out.println(newString(str));
 
 
+}
 
-
+    
+public static String readFile(){
+    File file = new File("Homework2/text.json");
+    Scanner scanner = null;
+    try {
+        scanner = new Scanner(file);
+    } catch (FileNotFoundException ex) {
+        System.out.println("Внимание! Ошибка:");
+        ex.printStackTrace();
     }
+    String res = "";
+    while (scanner.hasNext()){
+        res += scanner.nextLine() + "\n";
+    }
+    System.out.println(res);
+    return res;
+    }
+
+public static String newString(String str){
+    StringBuilder stringBuilder = new StringBuilder();
+    str = str.replace(":", ",")
+    .replaceAll("[^A-Za-zА-Яа-я-0-9-,]", "")
+    .replace("фамилия", "Студент")
+    .replace("оценка", "получил")
+    .replace("предмет", "по предмету");
+
+    String[] myString = str.split(",");
+    for (int i = 0; i < myString.length; i++) {
+        if (myString[i].equals("Студент") && i != 0) {
+            stringBuilder.append("\n");
+        }
+        stringBuilder.append(myString[i] + " ");
+        }   
+return stringBuilder.toString();
+}
+
+
 
 }
