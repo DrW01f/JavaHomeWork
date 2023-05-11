f77d7d67ee49977bbfb2774cd48651971384e46b
Andrey <Dr_Wo1f@mail.ru>
2023-05-11 23:58:07 +0300

    task 2 finished

--- homework3/Task2.java
+++ homework3/Task2.java
@@ -2,6 +2,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.IntSummaryStatistics;
 
 public class Task2 {
@@ -12,10 +13,13 @@ public class Task2 {
         ArrayList<Integer> list = new ArrayList<>();
         Collections.addAll(list, 1, 3, 8, 5, 9, 4, 3, 2, 7, 1, 6, 3, 8);
         Collections.max(list, null);
-        System.out.println(Collections.max(list));
-        System.out.println(Collections.min(list));
-
-
+        System.out.println("Максимальное значение: " + Collections.max(list));
+        System.out.println("Минимальное значение: " + Collections.min(list));
+        double sum = 0;
+        for (int x: list) {
+            sum += x;
+        }   
+        System.out.println("Среднее значение: " + sum/list.size());
     }
 
 
