0210f37ca2e6c001fe9c4e4416b4e1e6a018393a
Andrey <Dr_Wo1f@mail.ru>
2023-05-23 22:24:03 +0300

    task 2 started

---
+++ homework4/Task3.java
@@ -0,0 +1,8 @@
+public class Task3 {
+    /*Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке. 
+    Используйте итератор */
+    public static void main(String[] args) {
+        
+    }
+
+}
