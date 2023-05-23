7290479b7222aa6424eb4e568191cdd74e708b53
Andrey <Dr_Wo1f@mail.ru>
2023-05-23 23:01:20 +0300

    task 3 added

--- homework4/Task3.java
+++ homework4/Task3.java
@@ -1,8 +1,23 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Task3 {
     /*Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке. 
     Используйте итератор */
     public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 15; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
         
+        Integer sum = 0;
+        ListIterator<Integer> iterator = list.listIterator(list.size());
+            while (iterator.hasNext()){
+                sum += iterator.next() ;
+                System.out.println(iterator.next());                        
+            }
+        System.out.println(sum/list.size()); 
     }
 
 }
