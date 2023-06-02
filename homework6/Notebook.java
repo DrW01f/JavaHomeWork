b57f45cc57f95af674c92673cf973df9a3c4e589
Andrey <Dr_Wo1f@mail.ru>
2023-06-02 18:24:43 +0300

    homework 6 sterted

---
+++ homework6/Notebook.java
@@ -0,0 +1,18 @@
+package homework6;
+
+public class Notebook {
+    
+        
+    private Integer coast;
+    private Integer ramCount;
+    private Integer hardDriveCount;
+    private String operationSystem;
+    private String color;
+    
+
+
+
+            
+    
+   
+}
