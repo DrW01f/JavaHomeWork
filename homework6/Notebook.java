ac22bd22617d9dd6817fc303ac9b71d413088f92
Andrey <Dr_Wo1f@mail.ru>
2023-06-02 19:46:36 +0300

    in process

--- homework6/Notebook.java
+++ homework6/Notebook.java
@@ -1,18 +1,84 @@
-package homework6;
+import java.util.*;
 
 public class Notebook {
-    
+    /*Подумать над структурой класса Ноутбук для магазина техники -     
+выделить поля и методы. Реализовать в java.
+Создать множество ноутбуков.
+Написать метод, который будет запрашивать у пользователя критерий (или критерии) 
+фильтрации и выведет ноутбуки, отвечающие фильтру. Критерии фильтрации можно хранить в Map. 
+Например: “Введите цифру, соответствующую необходимому критерию:
+1 - ОЗУ
+2 - Объем ЖД
+3 - Операционная система
+4 - Цвет …
+Далее нужно запросить минимальные значения для указанных критериев - 
+сохранить параметры фильтрации можно также в Map.
+Отфильтровать ноутбуки из первоначального множества и вывести проходящие по условиям.
+     * 
+    */
         
     private Integer coast;
     private Integer ramCount;
     private Integer hardDriveCount;
     private String operationSystem;
     private String color;
-    
+    private Integer videoCardCapacity;
 
+    public Notebook(Integer coast, Integer ramCount, Integer hardDriveCount, String operationSystem, String color, Integer videoCardCapacity){
+        this.coast = coast;
+        this.ramCount = ramCount;
+        this.hardDriveCount = hardDriveCount;
+        this.operationSystem = operationSystem;
+        this.color = color;
+        this.videoCardCapacity = videoCardCapacity;
+    }
 
+    public Integer getcoast(){
+        return coast;
+    }
 
+    public Integer getramCount(){
+        return ramCount;
+    }
+
+    public Integer gethardDriveCount(){
+        return hardDriveCount;
+    }
+
+    public Integer getvideoCardCapacity(){
+        return videoCardCapacity;
+    }
+
+    public String getcolor(){
+        return color;
+    }
+
+    public String getoperationSystem(){
+        return operationSystem;
+    }
             
-    
+    public String toString() {
+        String result = String.format("coast: %s; ramCount: %s; hardDriveCount: %s; operationSystem: %s; color: %s; " +
+                "videoCardCapacity: %s.", coast, ramCount, hardDriveCount, operationSystem, color, videoCardCapacity);
+        return result;
+    }
    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof Notebook)){
+            return false;
+        }
+        Notebook note = (Notebook) obj;
+        return operationSystem.equals(note.operationSystem) && color.equals(note.color) && coast == note.coast && ramCount == note.ramCount && hardDriveCount == note. hardDriveCount && videoCardCapacity == note. videoCardCapacity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coast, ramCount, hardDriveCount, operationSystem, color, videoCardCapacity);
+    }
+
+
 }
--- homework6/Task1.java
+++ homework6/Task1.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class Task1 {
 /*Подумать над структурой класса Ноутбук для магазина техники -     
@@ -19,6 +19,49 @@ public class Task1 {
 
     public static void main(String[] args) {
 
-        
+    Notebook note1 = new Notebook(30_000, 4, 128, "windows7", "black", 2);
+    Notebook note2 = new Notebook(100_000, 16, 1024, "windows10", "white", 8);
+    Notebook note3 = new Notebook(200_000, 32, 2048, "windows11", "gold", 16);
+    Notebook note4 = new Notebook(70_000, 8, 512, "windows7", "black", 4);
+    Notebook note5 = new Notebook(50_000, 4, 256, "windows8", "black", 4);
+    Notebook note6 = new Notebook(90_000, 16, 512, "windows10", "white", 8);
+    Notebook note7 = new Notebook(60_000, 8, 256, "windows8", "black", 4);
+    Notebook note8 = new Notebook(30_000, 4, 128, "windows7", "black", 2);
+
+    Set<Notebook> notebookss = new HashSet<>();
+    notebookss.add(note1);
+    notebookss.add(note2);
+    notebookss.add(note3);
+    notebookss.add(note4);
+    notebookss.add(note5);
+    notebookss.add(note6);
+    notebookss.add(note7);
+    notebookss.add(note8);
+
+    Scanner scanner = new Scanner(System.in);
+    
+    while (true){
+        findMenu();
+        int filterNumber = scanner.nextInt();
+
     }
+
+
+    }
+    
+    public static void findMenu(){
+
+        System.out.println("Выберите условия для фильтра: \n");
+        System.out.println("1   Стоимость");
+        System.out.println("2   Объем оперативной памяти");
+        System.out.println("3   Объем жесткого диска");
+        System.out.println("4   Операционная система");
+        System.out.println("5   Цвет");
+        System.out.println("6   Объем видеокарты");
+        System.out.println("0   Выход");
+    }
+
+
 }
+    
+
